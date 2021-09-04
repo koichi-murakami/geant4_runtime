19b5c8bc63f7e421ac04cd3d5a925935fe212641
Koichi Murakami <koichi.murakami@kek.jp>
2021-09-05 03:03:06 +0900

    add buildx script for cross-building arm64 arch

---
+++ build_image_buildx.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -
+#
+# Build Docker image
+#
+
+export DOCKER_BUILDKIT=1
+
+echo "@@ Build Docker image ..."
+
+target=koichimurakamik6/geant4-runtime:10.7.2-mt
+
+id=`docker image ls -q $target`
+
+if [ -n "$id" ]; then
+  echo "@@ remove target image: $target ..."
+  docker image rm $target
+else
+  echo "@@ $target does not exist."
+fi
+
+echo "@@ build target image ..."
+docker buildx build --platform linux/amd64,linux/arm64 -f Dockerfile -t $target --push .
